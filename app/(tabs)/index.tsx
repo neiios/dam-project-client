9c22f4d24b3b6e8d0c9797ad1a71c0729f611d9d
tmneth <timeneth@gmail.com>
2024-06-10 15:23:17 +0100

    Add .env var with base api url

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,97 @@
+import React, { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
 import { Text, View, ScrollView, TouchableOpacity, Image } from "react-native";
-import { conferenceData } from "@/assets/data";
+
+interface Conference {
+  id: number;
+  name: string;
+  longitude: string;
+  latitude: string;
+  startDate: string;
+  endDate: string;
+  imageUrl: string;
+  description: string;
+  location?: string;
+  tracks?: string[];
+  city?: string;
+}
+
+const formatDateRange = (startDate: string, endDate: string) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const options: Intl.DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+  };
+  const startDay = start.getDate();
+  const endDay = end.getDate();
+  const month = new Intl.DateTimeFormat("en-US", { month: "short" }).format(
+    start
+  );
+
+  return `${month} ${startDay}-${endDay} ${end.getFullYear()}`;
+};
+
+const fetchCityName = async (
+  latitude: string,
+  longitude: string
+): Promise<string> => {
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&addressdetails=1`
+  );
+  const data = await response.json();
+  return (
+    data.address.city ||
+    data.address.town ||
+    data.address.village ||
+    "Unknown location"
+  );
+};
 
 export default function FeedScreen() {
   const router = useRouter();
+  const [conferenceData, setConferenceData] = useState<Conference[]>([]);
+
+  useEffect(() => {
+    const fetchConferenceData = async () => {
+      try {
+        const response = await fetch(
+          `http://${process.env.EXPO_PUBLIC_API_BASE}:8080/api/v1/conferences`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data: Conference[] = await response.json();
+
+        // Fetch city names for each conference
+        const dataWithCityNames = await Promise.all(
+          data.map(async (conference) => {
+            const cityName = await fetchCityName(
+              conference.latitude,
+              conference.longitude
+            );
+            return { ...conference, city: cityName };
+          })
+        );
+
+        setConferenceData(dataWithCityNames);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchConferenceData();
+  }, []);
+
   return (
     <ScrollView className="bg-white">
       <View>
+        <Text>sdf{process.env.API_BASE}</Text>
         <View className="flex w-full">
           {conferenceData.map((conference, index) => (
             <TouchableOpacity
-              key={conference.key}
+              key={conference.id}
               onPress={() => router.push(`/conference`)}
               className={`border-b-2 border-slate-100 ${
                 index === conferenceData.length - 1 ? "border-b-0" : ""
@@ -19,24 +100,24 @@ export default function FeedScreen() {
             >
               <View className="p-5 flex-row items-center justify-between">
                 <View className="flex gap-y-2 w-44">
-                  <Text className="text-lg font-bold">{conference.title}</Text>
+                  <Text className="text-lg font-bold">{conference.name}</Text>
                   <View className="flex flex-row gap-x-2">
-                    {conference.tracks.map((track) => {
-                      return (
+                    {conference.tracks &&
+                      conference.tracks.map((track) => (
                         <View className="bg-sky-100 rounded-md p-1" key={track}>
                           <Text className="text-xs font-semibold">{track}</Text>
                         </View>
-                      );
-                    })}
+                      ))}
                   </View>
                   <Text className="text-xs text-slate-500">
-                    {conference.date} • {conference.location}
+                    {formatDateRange(conference.startDate, conference.endDate)}{" "}
+                    • {conference.city}
                   </Text>
                 </View>
                 <Image
                   className="w-32 h-20 rounded-md"
                   source={{
-                    uri: conference.coverImgUrl,
+                    uri: conference.imageUrl,
                   }}
                 />
               </View>
