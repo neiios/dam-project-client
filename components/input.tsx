323171bbc1d318b24b3d74d9d5c50a10189af7c7
tmneth <timeneth@gmail.com>
2024-06-11 22:17:18 +0100

    Allow input to accept any props

--- components/input.tsx
+++ components/input.tsx
@@ -1,12 +1,18 @@
-import { TextInput } from "react-native";
+import React from "react";
+import { TextInput, TextInputProps } from "react-native";
 
-type InputProps = {
+type InputProps = TextInputProps & {
   length: number;
   placeholder: string;
   lines?: number;
 };
 
-export default function Input({ length, placeholder, lines }: InputProps) {
+export default function Input({
+  length,
+  placeholder,
+  lines,
+  ...props
+}: InputProps) {
   return (
     <TextInput
       multiline
@@ -14,8 +20,8 @@ export default function Input({ length, placeholder, lines }: InputProps) {
       maxLength={length}
       placeholder={placeholder}
       placeholderTextColor="#94a3b8"
-      style={{ textAlignVertical: "top" }}
       className="text-slate-400 flex box-content border-2 border-slate-100 focus:border-sky-200 rounded-md p-2"
+      {...props}
     />
   );
 }
