79509aa267c8090076e48dca8c76862b85572fc5
tmneth <timeneth@gmail.com>
2024-06-11 11:56:02 +0100

    Add search for articles

--- app/conference/articles.tsx
+++ app/conference/articles.tsx
@@ -37,19 +37,23 @@ export default function Articles() {
 
     setLoading(true);
     try {
-      await new Promise((resolve) => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
 
       const response = await fetch(
         `http://${
           process.env.EXPO_PUBLIC_API_BASE
         }:8080/api/v1/conferences/${id}/articles?pageSize=${pageSize}&page=${
           reset ? 1 : page
-        }`
+        }&searchTerm=${searchQuery}`
       );
       const data = await response.json();
-      setArticles((prevArticles) =>
-        reset ? data : [...prevArticles, ...data]
-      );
+
+      if (reset) {
+        setArticles(data);
+      } else {
+        setArticles((prevArticles) => [...prevArticles, ...data]);
+      }
+
       setHasMore(data.length === pageSize);
       setPage((prevPage) => (reset ? 2 : prevPage + 1));
     } catch (error) {
@@ -62,7 +66,7 @@ export default function Articles() {
 
   useEffect(() => {
     fetchArticles(true);
-  }, [id]);
+  }, [id, searchQuery]);
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
@@ -76,22 +80,6 @@ export default function Articles() {
   };
 
   const router = useRouter();
-
-  if (loading && !refreshing && articles.length === 0) {
-    return (
-      <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
-
-  if (error) {
-    return (
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-lg text-red-500">Error: {error}</Text>
-      </View>
-    );
-  }
 
   return (
     <ScrollView
@@ -107,7 +95,7 @@ export default function Articles() {
           handleLoadMore();
         }
       }}
-      scrollEventThrottle={5}
+      scrollEventThrottle={10}
     >
       <View>
         <View className="p-5 flex gap-y-4 border-b-2 border-slate-100">
@@ -117,8 +105,10 @@ export default function Articles() {
               onChangeText={onChangeQuery}
               placeholder="Search Articles"
               placeholderTextColor="#94a3b8"
-              editable={articles.length > 0}
-              className="text-slate-400 w-full box-content border-2 border-slate-100 focus:border-sky-200 rounded-md pl-10 py-2 "
+              editable={true}
+              className="text-slate-400 w-full box-content border-2 border-slate-100 focus:border-sky-200 rounded-md pl-10 py-2"
+              value={searchQuery}
+              onSubmitEditing={() => fetchArticles(true)}
             />
             <View className="absolute left-2">
               <AntDesign color="#94a3b8" name="search1" size={20} />
@@ -126,32 +116,32 @@ export default function Articles() {
           </View>
         </View>
         <View className="flex w-full">
-          {articles && articles.length > 0 ? (
-            articles.map((article, index) => (
-              <TouchableOpacity
-                key={article.id}
-                onPress={() => router.push(`/article`)}
-                className="border-b-2 border-slate-100"
-                activeOpacity={1}
-              >
-                <View className="flex w-full px-5 py-2">
-                  <View className="flex flex-row gap-x-2"></View>
-                  <Text className="text-lg font-bold mb-2">
-                    {article.title}
-                  </Text>
-                  <Text className="text-xs text-slate-500">
-                    {formatDate(article.startDate, article.endDate)}
+          {articles && articles.length > 0
+            ? articles.map((article, index) => (
+                <TouchableOpacity
+                  key={article.id}
+                  onPress={() => router.push(`/article`)}
+                  className="border-b-2 border-slate-100"
+                  activeOpacity={1}
+                >
+                  <View className="flex w-full px-5 py-2">
+                    <View className="flex flex-row gap-x-2"></View>
+                    <Text className="text-lg font-bold mb-2">
+                      {article.title}
+                    </Text>
+                    <Text className="text-xs text-slate-500">
+                      {formatDate(article.startDate, article.endDate)}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ))
+            : !loading && (
+                <View className="p-5">
+                  <Text className="text-lg text-center text-slate-500">
+                    No articles available
                   </Text>
                 </View>
-              </TouchableOpacity>
-            ))
-          ) : (
-            <View className="p-5">
-              <Text className="text-lg text-center text-slate-500">
-                No articles available
-              </Text>
-            </View>
-          )}
+              )}
           {hasMore && (
             <View className="p-5 h-20">
               {loading && <ActivityIndicator size="large" color="#075985" />}
