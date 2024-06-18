35220f482f7de9d978f4da5c81f537167484e12f
Igor <igor@sgf.lt>
2024-06-18 12:16:53 +0100

    Add edit and add article pages

---
+++ app/admin/conferences/[conferenceId]/articles/[articleId]/index.tsx
@@ -0,0 +1,227 @@
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useAuth } from "@/app/context/AuthContext";
+
+import Button from "@/components/button";
+import {
+  DateTimePickerAndroid,
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function Page() {
+  const router = useRouter();
+
+  const { isAuthenticated, userRole } = useAuth();
+  useEffect(() => {
+    if (!isAuthenticated || userRole !== "admin") {
+      router.replace("/auth");
+    }
+  }, []);
+
+  const params = useLocalSearchParams();
+  const conferenceId = params.conferenceId;
+  const articleId = params.articleId;
+
+  const [article, setArticle] = useState({
+    title: "",
+    authors: "",
+    abstract: "",
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+
+  useEffect(() => {
+    async function fetchArticleData() {
+      if (!articleId) return;
+
+      try {
+        const jwtToken = await AsyncStorage.getItem("jwtToken");
+        const response = await fetch(
+          `http://${process.env.EXPO_PUBLIC_API_BASE}/api/v1/conferences/${conferenceId}/articles/${articleId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setArticle({
+            title: data.title,
+            authors: data.authors,
+            abstract: data.abstract,
+            startDate: new Date(data.startDate),
+            endDate: new Date(data.endDate),
+          });
+        } else {
+          Alert.alert("Error", "Failed to fetch article data.");
+        }
+      } catch (error) {
+        Alert.alert("Error", "An unexpected error occurred.");
+      }
+    }
+
+    fetchArticleData();
+  }, [articleId]);
+
+  async function handleSubmit() {
+    try {
+      const jwtToken = await AsyncStorage.getItem("jwtToken");
+      const response = await fetch(
+        `http://${process.env.EXPO_PUBLIC_API_BASE}/api/v1/conferences/${conferenceId}/articles/${articleId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${jwtToken}`,
+          },
+          body: JSON.stringify(article),
+        }
+      );
+
+      if (response.ok) {
+        Alert.alert("Success", "Article updated successfully!");
+        router.push({
+          pathname: "/(tabs)",
+          params: { confId: conferenceId },
+        });
+      } else {
+        Alert.alert("Error", "Article update failed.");
+        console.log(await response.json());
+      }
+    } catch (error) {
+      Alert.alert("Error", "An unexpected error occurred.");
+    }
+  }
+
+  function showStartDatePicker(mode: any) {
+    function updateStartDate(
+      event: DateTimePickerEvent,
+      selectedDate: Date | undefined
+    ) {
+      if (!selectedDate) {
+        return;
+      }
+      setArticle({ ...article, startDate: selectedDate });
+    }
+
+    DateTimePickerAndroid.open({
+      value: article.startDate,
+      onChange: updateStartDate,
+      mode: mode,
+      is24Hour: true,
+    });
+  }
+
+  function showEndDatePicker(mode: any) {
+    function updateEndDate(
+      event: DateTimePickerEvent,
+      selectedDate: Date | undefined
+    ) {
+      if (!selectedDate) {
+        return;
+      }
+      setArticle({ ...article, endDate: selectedDate });
+    }
+
+    DateTimePickerAndroid.open({
+      value: article.endDate,
+      onChange: updateEndDate,
+      mode: mode,
+      is24Hour: true,
+    });
+  }
+
+  return (
+    <SafeAreaView className="h-full bg-white flex-1">
+      <ScrollView contentContainerStyle={{ padding: 20 }}>
+        <Text className="font-bold text-2xl mb-4">Edit Article</Text>
+
+        <TextInput
+          className="w-full border border-gray-300 p-2 rounded-md mb-3"
+          placeholder="Article Title"
+          value={article.title}
+          onChangeText={(text) => setArticle({ ...article, title: text })}
+        />
+
+        <TextInput
+          className="w-full border border-gray-300 p-2 rounded-md mb-3"
+          placeholder="Authors (comma-separated)"
+          value={article.authors}
+          onChangeText={(text) => setArticle({ ...article, authors: text })}
+        />
+
+        <TextInput
+          className="w-full border border-gray-300 p-2 rounded-md mb-3"
+          placeholder="Abstract"
+          value={article.abstract}
+          multiline={true}
+          numberOfLines={4}
+          onChangeText={(text) => setArticle({ ...article, abstract: text })}
+        />
+
+        <View className="flex-row justify-between mb-4">
+          <View>
+            <Button
+              title="Pick Start Date"
+              onPress={() => showStartDatePicker("date")}
+            />
+            <Text className="text-center">
+              {article.startDate.toLocaleDateString()}
+            </Text>
+          </View>
+
+          <View>
+            <Button
+              title="Pick Start Time"
+              onPress={() => {
+                showStartDatePicker("time");
+              }}
+            />
+            <Text className="text-center">
+              {article.startDate.toLocaleTimeString()}
+            </Text>
+          </View>
+        </View>
+
+        <View className="flex-row justify-between mb-4">
+          <View>
+            <Button
+              title="Pick End Date"
+              onPress={() => showEndDatePicker("date")}
+            />
+            <Text className="text-center mt-2">
+              {article.endDate.toLocaleDateString()}
+            </Text>
+          </View>
+
+          <View>
+            <Button
+              onPress={() => {
+                showEndDatePicker("time");
+              }}
+              title="Pick End Time"
+            />
+            <Text className="text-center">
+              {article.endDate.toLocaleTimeString()}
+            </Text>
+          </View>
+        </View>
+
+        <Button title="Submit" onPress={handleSubmit} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
--- app/article.tsx
+++ app/article.tsx
@@ -1,6 +1,6 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import { useRoute } from "@react-navigation/native";
 import { useFetchData } from "@/core/hooks";
 import { Article } from "@/types";
@@ -8,6 +8,8 @@ import { formatDate } from "@/core/utils";
 import Loader from "@/components/loader";
 import Title from "@/components/title";
 import Header from "@/components/header";
+import { useAuth } from "./context/AuthContext";
+import { router } from "expo-router";
 
 export default function ArticleDetails() {
   const route = useRoute();
@@ -15,6 +17,8 @@ export default function ArticleDetails() {
     articleId: string;
     confId: string;
   };
+
+  const { isAuthenticated, userRole } = useAuth();
 
   const {
     data: article,
@@ -37,52 +41,68 @@ export default function ArticleDetails() {
   }
 
   return (
-    <ScrollView className="bg-white">
-      <View>
+    <View className="min-h-full bg-white">
+      <ScrollView>
         <View>
-          <Header>
-            <Title>{article?.title}</Title>
-          </Header>
-          <View className="p-5 flex gap-y-5">
-            <View>
-              <Text className="text-lg font-bold mb-2">Abstract</Text>
-              <Text className="text-lg">{article?.abstract}</Text>
-            </View>
-            <View>
-              <Text className="text-lg font-bold mb-2">Date and time</Text>
-              <View className="flex flex-row gap-x-2">
-                <AntDesign name="calendar" size={20} />
-                <Text className="font-semibold">
-                  {formatDate(article!.startDate, article!.endDate)}
-                </Text>
+          <View>
+            <Header>
+              <Title>{article?.title}</Title>
+            </Header>
+            <View className="p-5 flex gap-y-5">
+              <View>
+                <Text className="text-lg font-bold mb-2">Abstract</Text>
+                <Text className="text-lg">{article?.abstract}</Text>
               </View>
-            </View>
-            <View>
-              <Text className="text-lg font-bold mb-2">Speakers</Text>
-              <View className="flex flex-row gap-x-2">
-                <AntDesign name="user" size={20} />
-                <Text className="font-semibold">{article?.authors}</Text>
+              <View>
+                <Text className="text-lg font-bold mb-2">Date and time</Text>
+                <View className="flex flex-row gap-x-2">
+                  <AntDesign name="calendar" size={20} />
+                  <Text className="font-semibold">
+                    {formatDate(article!.startDate, article!.endDate)}
+                  </Text>
+                </View>
               </View>
-            </View>
+              <View>
+                <Text className="text-lg font-bold mb-2">Speakers</Text>
+                <View className="flex flex-row gap-x-2">
+                  <AntDesign name="user" size={20} />
+                  <Text className="font-semibold">{article?.authors}</Text>
+                </View>
+              </View>
 
-            <View>
-              <Text className="text-lg font-bold mb-2">Room</Text>
-              <View className="flex flex-row gap-x-2">
-                <AntDesign name="find" size={20} />
-                <Text className="font-semibold">{article?.track.room}</Text>
+              <View>
+                <Text className="text-lg font-bold mb-2">Room</Text>
+                <View className="flex flex-row gap-x-2">
+                  <AntDesign name="find" size={20} />
+                  <Text className="font-semibold">{article?.track.room}</Text>
+                </View>
               </View>
-            </View>
 
-            <View>
-              <Text className="text-lg font-bold mb-2">Track</Text>
-              <View className="flex flex-row gap-x-2">
-                <AntDesign name="paperclip" size={20} />
-                <Text className="font-semibold">{article?.track.name}</Text>
+              <View>
+                <Text className="text-lg font-bold mb-2">Track</Text>
+                <View className="flex flex-row gap-x-2">
+                  <AntDesign name="paperclip" size={20} />
+                  <Text className="font-semibold">{article?.track.name}</Text>
+                </View>
               </View>
             </View>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+
+      {isAuthenticated && userRole === "admin" ? (
+        <View className="absolute bottom-8 right-8 flex items-center">
+          <TouchableOpacity
+            className="bg-sky-700 py-4 px-4 rounded-xl w-full"
+            activeOpacity={0.8}
+            onPress={() =>
+              router.push(`/admin/conferences/${confId}/articles/${articleId}`)
+            }
+          >
+            <MaterialIcons color="white" name="edit" size={32} />
+          </TouchableOpacity>
+        </View>
+      ) : null}
+    </View>
   );
 }
