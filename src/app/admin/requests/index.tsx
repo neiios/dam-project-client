7bdd04980b4b90010914eb629d93e4735e420448
Igor <igor@sgf.lt>
2024-06-19 10:56:15 +0100

    Improve requests page layout

--- src/app/admin/requests/index.tsx
+++ src/app/admin/requests/index.tsx
@@ -15,6 +15,8 @@ export default function Page() {
   }, [isAuthenticated, userRole]);
 
   const [requests, setRequests] = useState<Request[]>([]);
+  const pendingRequests = requests.filter((r) => r.status === "pending");
+  const answeredRequests = requests.filter((r) => r.status === "answered");
 
   useEffect(() => {
     async function fetchRequests() {
@@ -61,45 +63,79 @@ export default function Page() {
 
   return (
     <ScrollView className="bg-white min-h-full px-10">
-      <Text className="text-4xl font-bold mb-8 mt-10 text-center">
-        Requests
-      </Text>
-      <View className="flex items-center">
-        {requests.map((request) => (
-          <View
-            key={request.id}
-            className="border border-neutral-300 p-4 rounded-lg shadow-md flex items-center w-full mb-4"
-          >
-            <View className="border-b border-neutral-300 w-full">
-              <Text className="text-xl text-center font-bold">
-                Question: {request.question}
-              </Text>
+      <View className="flex-col items-center">
+        <Text className="text-4xl font-bold mb-8 mt-10 text-center">
+          Requests
+        </Text>
 
-              {request.status === "answered" ? (
-                <Text className="text-lg mt-4 mb-4 text-center ">
-                  Answer: {request.answer}
+        <Text className="text-2xl font-bold mb-4">Pending Requests</Text>
+
+        <View className="flex items-center w-full">
+          {pendingRequests.map((request) => (
+            <View
+              key={request.id}
+              className="border border-neutral-300 p-4 rounded-lg shadow-md flex items-center w-full mb-4"
+            >
+              <View className="border-b border-neutral-300 w-full">
+                <Text className="text-xl text-center font-bold mb-4">
+                  {request.question}
                 </Text>
-              ) : null}
-            </View>
 
-            <View className="w-full mt-4">
-              <View>
-                <Button
-                  title="Answer"
-                  onPress={() => router.push(`/admin/requests/${request.id}`)}
-                />
+                <Text className="text-lg mb-4 text-center ">
+                  Status: Waiting for an answer
+                </Text>
               </View>
 
-              <View className="mt-4">
-                <Button
-                  title="Remove"
-                  bgColor="bg-red-500"
-                  onPress={() => handleRemove(request.id)}
-                />
+              <View className="flex-row gap-x-4 p-4">
+                <View>
+                  <Button
+                    title="Remove"
+                    bgColor="bg-red-500"
+                    onPress={() => handleRemove(request.id)}
+                  />
+                </View>
+
+                <View>
+                  <Button
+                    title="Answer"
+                    onPress={() => router.push(`/admin/requests/${request.id}`)}
+                  />
+                </View>
               </View>
             </View>
-          </View>
-        ))}
+          ))}
+
+          <Text className="text-2xl font-bold mb-4">Answered Requests</Text>
+
+          {answeredRequests.map((request) => (
+            <View
+              key={request.id}
+              className="border border-neutral-300 p-4 rounded-lg shadow-md flex items-center w-full mb-4"
+            >
+              <View className="border-b border-neutral-300 w-full">
+                <Text className="text-xl text-center font-bold mb-4">
+                  {request.question}
+                </Text>
+
+                {request.status === "answered" ? (
+                  <Text className="text-lg mb-4 text-center ">
+                    Answer: {request.answer}
+                  </Text>
+                ) : null}
+              </View>
+
+              <View className="flex-row gap-x-4 p-4">
+                <View>
+                  <Button
+                    title="Remove"
+                    bgColor="bg-red-500"
+                    onPress={() => handleRemove(request.id)}
+                  />
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
